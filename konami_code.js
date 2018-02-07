66f3f24d9928db1232db25f494886e2d8f7a9a83
arye dov eidelman <aryedov.e@gmail.com>
2018-02-07 00:31:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,23 +5,21 @@ function init() {
   var body = document.querySelector('body');
   body.addEventListener('keydown', function onKeyDownHandler(event) {
     var e = event;
-    console.log(`key:${key}`);
-    console.log(`e:${e}`);
-    console.log(`code:${code}`);
     const key = parseInt(e.detail || e.which);
     var index = 0;
     if (key === code[index]) {
       index++;
-
       if (index === code.length) {
         console.log(e);
         alert("Hurray!");
-
         index = 0;
       }
     } else {
       index = 0;
     }
+    console.log(`key:${key}`);
+    console.log(`e:${e}`);
+    console.log(`index:${index}`);
   })
 }
 init()
