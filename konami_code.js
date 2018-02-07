25374dc8ae319a7d41f3d95eadc5cf76f213a49c
arye dov eidelman <aryedov.e@gmail.com>
2018-02-07 00:37:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  console.log('a');
+var index = 0;
   var body = document.querySelector('body');
   body.addEventListener('keydown', function onKeyDownHandler(event) {
     var e = event;
     const key = parseInt(e.detail || e.which);
-    var index = 0;
     if (key === code[index]) {
       index++;
       if (index === code.length) {
